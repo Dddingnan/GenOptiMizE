1bf8f4edabba5c11836fbe43dfbc57b4f2d393a5
dddingnan <silenceapple1027@gmail.com>
2023-03-30 13:10:05 -0400

    feat: define data format.

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,6 +1,26 @@
-export const aa = [
-  {
-    aa: 123,
-    bb: 456,
+export const aa = {
+  rMfetesPW8WeC3SlfSvjq17Ei5v2: {
+    enzyme: [
+      {
+        enzyme: '2C19',
+        diploType: '17*17',
+        phenoType: 'Ultra-rapid metabolizer',
+      },
+      {
+        enzyme: '2D6',
+        diploType: '3*4',
+        phenoType: 'Poor metabolizer',
+      },
+    ],
+    medicine: [
+      {
+        name: 'A',
+        dose: 'Dose 1 s1dadasd',
+      },
+      {
+        name: 'B',
+        dose: 'Dose 2 s1dadasd',
+      },
+    ],
   },
-];
+};
